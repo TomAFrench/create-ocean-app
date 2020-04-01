fc95dab26d5707ad42f6419da95b96a4efd72b86
Tom French <tom@tomfren.ch>
2020-04-01 15:51:24 +0100

    refactor: move cleanupContentType to utils folder

--- templates/default/packages/react-app/src/components/publish/Essentials.js
+++ templates/default/packages/react-app/src/components/publish/Essentials.js
@@ -6,52 +6,9 @@ import Button from '@material-ui/core/Button';
 import filesize from 'filesize'
 import Dotdotdot from 'react-dotdotdot'
 import axios from 'axios'
+import cleanupContentType from '../../utils/cleanupContentType'
 
 const Ipfs = lazy(() => import('../IpfsDropzone'))
-
-const cleanupContentType = (contentType: string) => {
-  // strip away the 'application/' part
-  const contentTypeSplit = contentType.split('/')[1]
-
-  if (!contentTypeSplit) return contentType
-
-  let contentTypeCleaned
-
-  // TODO: add all the possible archive & compression MIME types
-  switch (contentType) {
-      case 'application/x-lzma':
-      case 'application/x-xz':
-      case 'application/x-tar':
-      case 'application/x-gtar':
-      case 'application/x-bzip2':
-      case 'application/x-gzip':
-      case 'application/x-7z-compressed':
-      case 'application/x-rar-compressed':
-      case 'application/x-zip-compressed':
-      case 'application/x-apple-diskimage':
-          contentTypeCleaned = contentTypeSplit
-              .replace('x-', '')
-              .replace('-compressed', '')
-          break
-      default:
-          contentTypeCleaned = contentTypeSplit
-          break
-  }
-
-  // Manual replacements
-  contentTypeCleaned = contentTypeCleaned
-      .replace(
-          'vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-          'xlsx'
-      )
-      .replace('vnd.ms-excel', 'xls')
-      .replace('apple-diskimage', 'dmg')
-      .replace('octet-stream', 'Binary')
-      .replace('svg+xml', 'svg')
-
-  return contentTypeCleaned
-}
-
 
 const Item = ({ item, removeFile }) => (
     <li>
